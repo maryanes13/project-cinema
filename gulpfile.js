2724bc919461c0a23b3201e91531e8d79eab38b2
Alexey <maryanespanteleeva@yandex.ru>
2024-12-30 13:02:13 +0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,203 @@
+import gulp from "gulp";
+import autoprefixer from "autoprefixer";
+import del from "del";
+import squoosh from "gulp-libsquoosh";
+import terser from "gulp-terser";
+import webp from "gulp-webp";
+import sourcemap from "gulp-sourcemaps";
+import plumber from "gulp-plumber";
+import postcss from "gulp-postcss";
+import svgstore from "gulp-svgstore";
+import csso from "postcss-csso";
+import rename from "gulp-rename";
+import sync from "browser-sync";
+import sass from 'gulp-dart-sass';
+import webphtml from "gulp-webp-html";
+import fileinclude from "gulp-file-include";
+import formatHTML from 'gulp-format-html';
+import ttf2woff from 'gulp-ttf2woff';
+import ttf2woff2 from 'gulp-ttf2woff2';
+import fonter from 'gulp-fonter';
+const ghPages = require('gh-pages');
+const path = require('path');
+
+//deploy
+ghPages.publish('dist', function(err) {});
+
+//resourses
+const vendors = () => {
+    return gulp.src("source/vendors/**", { encoding: false })
+      .pipe(gulp.dest('build/vendors'))
+  }
+
+//fonts
+ const fonts =() => {
+    gulp.src("source/fonts/*.otf")
+    .pipe(fonter({
+        formats: ['ttf']
+     }))
+    .pipe(gulp.dest('source/fonts'));
+    gulp.src("source/fonts/*.ttf2")
+    .pipe(ttf2woff2())
+    .pipe(gulp.dest('build/fonts'));
+    return gulp.src("source/fonts/*.ttf")
+    .pipe(ttf2woff())
+    .pipe(gulp.dest('build/fonts'));
+ };
+
+//WebP
+export const createWebp = (done) => {
+    gulp.src('source/img/*.{png,jpg,tiff}')
+	.pipe(webp({quality: 90}))
+	.pipe(gulp.dest('build/img'))
+    done();
+}
+
+
+//Styles
+const styles = () => {
+    return gulp.src("source/sass/style.scss")
+    .pipe(plumber())
+    .pipe(sourcemap.init())
+    .pipe(sass())
+    .pipe(gulp.dest("build/css"))
+    .pipe(postcss([
+        autoprefixer(),
+        csso()
+    ]))
+    .pipe(rename("style.min.css"))
+    .pipe(sourcemap.write("."))
+    .pipe(gulp.dest("build/css"))
+    .pipe(sync.stream());
+}
+
+//Html
+const html = () => {
+ return gulp.src("source/*.html")
+    .pipe(fileinclude())
+    .pipe(webphtml())
+    .pipe(formatHTML())
+    .pipe(gulp.dest("build"))
+    .pipe(sync.stream());
+}
+
+//Scripts
+const scripts = () => {
+    return gulp.src("source/js/main.js")
+    .pipe(gulp.dest("build/js"))
+    .pipe(terser())
+    .pipe(rename("main.min.js"))
+    .pipe(gulp.dest("build/js"))
+    .pipe(sync.stream());
+}
+
+
+//Image
+const OptimizeImage = () => {
+    return gulp.src('source/img/*.{png,jpg,svg}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'))
+}
+
+
+//copy Image
+const copyImage = () => {
+    return gulp.src('source/img/*.{png,jpg,svg}')
+    .pipe(gulp.dest('build/img'))
+    .pipe(sync.stream());
+}
+
+//Sprite
+const sprite = () => {
+    return gulp.src("source/img/**/*.svg")
+    .pipe(svgstore(
+        { inlineSvg: true })
+        )
+    .pipe(rename("sprite.svg"))
+    .pipe(gulp.dest("build/sprite"))
+}
+
+
+//Copy
+const copy = () => {
+    return gulp.src([
+        "source/fonts/*.{woff,woff2}",
+        "source/*.ico",
+        "source/img/**/*.svg",
+        "!source/img/icons/*.svg"
+    ], {
+        base: "source"
+        })
+    .pipe(gulp.dest("build"))
+    done();
+}
+
+
+// Clean
+const clean = () => {
+    return del("build");
+  };
+
+// //Server
+const server = (done) => {
+    sync.init({
+      server: {
+        baseDir: "build"
+      },
+      cors: true,
+      notify: false,
+      ui: false,
+    });
+    done();
+  }
+
+
+// Reload
+const reload = (done) => {
+    sync.reload();
+    done();
+}
+
+//Watcher
+const watcher = () => {
+    gulp.watch("source/sass/**/*.scss", gulp.series(styles));
+    gulp.watch("source/js/main.js", gulp.series(scripts));
+    gulp.watch('source/**/*.html', gulp.series(html,reload));
+    gulp.watch('source/img', copyImage);
+}
+
+//Build
+export const build = gulp.series(
+    clean,
+    copy,
+    OptimizeImage,
+    gulp.parallel (
+        styles,
+        html,
+        scripts,
+        createWebp,
+        sprite,
+        fonts,
+        vendors
+    ),
+);
+
+//Default
+export default gulp.series(
+    clean,
+    copy,
+    copyImage,
+    gulp.parallel (
+        styles,
+        html,
+        scripts,
+        sprite,
+        createWebp,
+        fonts,
+        vendors
+    ),
+    gulp.series(
+        server,
+        watcher
+    )
+)
---
+++ source/js/main.js
@@ -0,0 +1,416 @@
+// const { document } = require("postcss");
+
+let navPage = document.querySelector('.page-header__nav');
+let navToggle = document.querySelector('.burger-button');
+let navClosed = document.querySelector('.button-closed');
+let navFooter = document.querySelector(".footer-navigation")
+
+// current-item
+if(navPage) {
+
+  const links = navPage.getElementsByTagName('a');
+  for (let i = 0; i < links.length; i++) {
+
+    if (links[i].href === window.location.href) {
+      console.log(links[i].href)
+      links[i].classList.add('current-item');
+    }
+
+
+  }
+}
+
+if(navFooter) {
+  const links = navFooter.getElementsByTagName('a');
+  for (let i = 0; i < links.length; i++) {
+
+    if (links[i].href === window.location.href) {
+      console.log(links[i].href)
+      links[i].classList.add('footer-current-item');
+      break;
+    }
+  }
+}
+
+// Menu
+if(navPage) {
+  navToggle.addEventListener('click', function() {
+    if (navPage.classList.contains('active')) {
+      navPage.classList.remove('active');
+    } else {
+      navPage.classList.add('active');
+    }
+  });
+
+  navClosed.addEventListener('click', function() {
+    if (navPage.classList.contains('active')) {
+      navPage.classList.remove('active');
+    }
+  });
+}
+
+
+
+  //Slider
+  if(document.querySelector(".courses-slider")) {
+
+    let swiper = new Swiper(".courses-slider", {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+      },
+      watchOverflow: false,
+      slidesPerGroup: 1,
+      breakpoints: {
+        300: {
+          slidesPerView: 1,
+       },
+        480: {
+           slidesPerView: 1,
+        },
+        768: {
+           slidesPerView: 2,
+        },
+        1280: {
+           slidesPerView: 3,
+        }
+     }
+    });
+  }
+
+  if(document.querySelector(".professors-slider")) {
+
+    let swiper2 = new Swiper(".professors-slider", {
+      slidesPerView: 3,
+      spaceBetween: 0,
+      navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+      },
+      watchOverflow: false,
+      slidesPerGroup: 1,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+       },
+        480: {
+           slidesPerView: 1,
+        },
+        768: {
+           slidesPerView: 1.5,
+        },
+        900: {
+          slidesPerView: 1.8,
+       },
+        1280: {
+           slidesPerView: 2.5,
+        }
+     }
+    });
+  }
+
+  if(document.querySelector(".swiper-reviews")) {
+    let swiper3 = new Swiper(".swiper-reviews", {
+      slidesPerView: 2.8,
+      spaceBetween: 25,
+      watchOverflow: false,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+       },
+        480: {
+           slidesPerView: 1,
+        },
+        768: {
+           slidesPerView: 1.5,
+        },
+        900: {
+          slidesPerView: 2,
+       },
+        1200: {
+           slidesPerView: 2.5,
+        }
+     }
+    });
+  }
+
+  if(document.querySelector(".course-teacher__slider")) {
+    let swiper4 = new Swiper(".course-teacher__slider", {
+      effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: false,
+        initialSlide: 0,
+        slidesPerView: "auto",
+        coverflowEffect: {
+          rotate: 10,
+          stretch: 500,
+          depth: 200,
+          modifier: 1,
+          slideShadows: true,
+        },
+        loop: true,
+        breakpoints: {
+          400: {
+            coverflowEffect: {
+              stretch: 100,
+              rotate: 10,
+              depth: 200,
+              modifier: 1,
+            },
+          },
+          800: {
+            coverflowEffect: {
+              stretch: 300,
+              rotate: 10,
+              depth: 200,
+              modifier: 1,
+            },
+          },
+          1300: {
+            coverflowEffect: {
+              rotate: 10,
+              stretch: 600,
+              depth: 200,
+              modifier: 1,
+            },
+          },
+        }
+    });
+  }
+
+  if(document.querySelector(".page-courses-slider")) {
+    let swiper5 = new Swiper(".page-courses-slider", {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      initialSlide: 2,
+      centeredSlides: true,
+      speed: 1000,
+      freeMode: false,
+      grabCursor: true,
+      navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+      },
+      watchOverflow: false,
+      slidesPerGroup: 1,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+       },
+        768: {
+           slidesPerView: "auto",
+        },
+      }
+    });
+  }
+
+  if(document.querySelector(".teacher-page-sliders")) {
+    let swiper6 = new Swiper(".sliders-faculty-one", {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+      },
+      watchOverflow: false,
+      slidesPerGroup: 1,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+       },
+        480: {
+           slidesPerView: 1,
+        },
+        768: {
+           slidesPerView: 1,
+        },
+        900: {
+          slidesPerView: 2,
+       },
+        1280: {
+           slidesPerView: 3,
+        }
+     }
+    });
+
+    let swiper7 = new Swiper(".sliders-faculty-second", {
+      slidesPerView: 3,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+      },
+      watchOverflow: false,
+      slidesPerGroup: 1,
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+       },
+        480: {
+           slidesPerView: 1,
+        },
+        768: {
+           slidesPerView: 1,
+        },
+        900: {
+          slidesPerView: 2,
+       },
+        1300: {
+           slidesPerView: 3,
+        }
+     }
+    });
+  }
+
+  if(document.querySelector(".about-gallery-wrapper")) {
+    let swiper8 = new Swiper(".about-gallery-slider", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      initialSlide: 0,
+      navigation: {
+        nextEl: ".swiper-next",
+        prevEl: ".swiper-prev",
+      },
+      watchOverflow: false,
+      slidesPerGroup: 1,
+      breakpoints: {
+        300: {
+          slidesPerView: 1,
+       },
+        480: {
+           slidesPerView: 1,
+        },
+        768: {
+           slidesPerView: 1,
+        },
+        900: {
+          slidesPerView: 2,
+       },
+        1280: {
+           slidesPerView: 3,
+        }
+     }
+    });
+
+  }
+//Accordeon
+
+if(document.querySelector(".accordeon-list")) {
+
+  const accordeonItems = document.querySelectorAll(".accordion-list__item");
+
+  for (let i = 0; i < accordeonItems.length; i++) {
+    accordeonItems[i].addEventListener('click', function(el) {
+
+      el.currentTarget.classList.toggle('active');
+
+      for (let j = 0; j < accordeonItems.length; j++) {
+        if (accordeonItems[j] !== el.currentTarget && accordeonItems[j].className === "accordion-list__item active") {
+          accordeonItems[j].classList.remove('active');
+        }
+      }
+    });
+  };
+}
+
+//Tabs
+
+if(document.querySelector(".section-tabs")) {
+
+  const tabs = document.querySelectorAll(".tabs-button ");
+  const contents = document.querySelectorAll(".tabs-panel");
+
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", ( event ) => {
+
+      let tabsChildren = event.target.parentElement.children;
+      console.log(tabsChildren)
+      for (let t = 0; t < tabsChildren.length; t++) {
+        tabsChildren[t].classList.remove("tabs-button-active");
+      }
+
+      tabs[i].classList.add("tabs-button-active");
+
+      let tabContentChildren = event.target.parentElement.nextElementSibling.children;
+      for (let c = 0; c < tabContentChildren.length; c++) {
+        tabContentChildren[c].classList.remove("tabs-panel-show");
+      }
+
+      contents[i].classList.add("tabs-panel-show");
+    });
+  }
+}
+
+if(document.querySelector(".section-courses-slider")) {
+
+  const tabs = document.querySelectorAll(".section-directions__item");
+  const contents = document.querySelectorAll(".section-directions-panel");
+
+  for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", ( event ) => {
+
+      let tabsChildren = event.target.parentElement.children;
+      console.log(tabsChildren)
+      for (let t = 0; t < tabsChildren.length; t++) {
+        tabsChildren[t].classList.remove("directions-active");
+      }
+
+      tabs[i].classList.add("directions-active");
+
+      let tabContentChildren = event.target.parentElement.nextElementSibling.children;
+      console.log(tabContentChildren)
+
+      for (let c = 0; c < tabContentChildren.length; c++) {
+        tabContentChildren[c].classList.remove("section-directions-panel-show");
+      }
+
+      contents[i].classList.add("section-directions-panel-show");
+    });
+  }
+}
+
+//Modal
+if(document.querySelector(".thanks-content")) {
+  document.addEventListener("DOMContentLoaded", function(){
+    let scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+    console.log(scrollbar);
+    document.querySelector('[href="#openModal"]').addEventListener('click',function(){
+      document.body.style.overflow = 'hidden';
+      document.querySelector('#openModal').style.marginLeft = scrollbar;
+    });
+    document.querySelector('[href="#close"]').addEventListener('click',function(){
+      document.body.style.overflow = 'visible';
+      document.querySelector('#openModal').style.marginLeft = '0px';
+    });
+  });
+}
+
+// Progress-bar
+if(document.querySelector(".progress-bar")) {
+
+  function progressBar() {
+    let historyContainer = document.querySelector(".about-history")
+    let scroll = document.body.scrollTop || document.documentElement.scrollTop;
+    let height = historyContainer.offsetHeight;
+    let scrolled = (scroll / height * 100) - 100;
+    let progressNow = document.querySelector(".progress-bar-now")
+    progressNow.style.height = scrolled + '%';
+
+    if(scrolled >= 50) {
+      document.querySelector(".progress-dots__second").classList.add("dots-active")
+    } else {
+      document.querySelector(".progress-dots__second").classList.remove("dots-active")
+    }
+    if (scrolled >= 95) {
+      document.querySelector(".progress-dots__third").classList.add("dots-active")
+      progressNow.style.height = 100 + '%'
+    } else {
+      document.querySelector(".progress-dots__third").classList.remove("dots-active")
+      progressNow.style.height = scrolled + '%';
+    }
+}
+
+  window.addEventListener('scroll', progressBar);
+}
