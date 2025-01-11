326a0ed6247573dfc31e657e957eeb2feb4f2d74
Alexey <maryanespanteleeva@yandex.ru>
2025-01-12 03:06:19 +0700

    Updates

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ window.addEventListener('DOMContentLoaded', () => {
   let navFooter = document.querySelector(".footer-navigation");
   let animItems = document.querySelectorAll(".anim-items")
 
-  if(animItems.length > 0) {
+  if (animItems.length > 0) {
     window.addEventListener("scroll", animOnScroll);
     function animOnScroll(params) {
-      for(let index = 0; index < animItems.length; index++) {
+      for (let index = 0; index < animItems.length; index++) {
         const animItem = animItems[index];
         const animItemHeight = animItem.offsetHeight;
         const animItemOffset = offset(animItem).top;
@@ -18,11 +18,11 @@ window.addEventListener('DOMContentLoaded', () => {
 
         let animItemPoint = window.innerHeight - animItemHeight / animStart;
 
-        if(animItemHeight > window.innerHeight) {
+        if (animItemHeight > window.innerHeight) {
           animItemPoint = window.innerHeight - window.innerHeight / animStart;
         }
 
-        if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
           animItem.classList.add("anim-active")
         } else {
           animItem.classList.remove("anim-active")
@@ -35,7 +35,7 @@ window.addEventListener('DOMContentLoaded', () => {
       const rect = el.getBoundingClientRect(),
         scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
         scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-      return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+      return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
     }
 
     setTimeout(() => {
@@ -48,9 +48,9 @@ window.addEventListener('DOMContentLoaded', () => {
   window.addEventListener("scroll", () => {
     let scrollTop = window.scrollY;
 
-    if(scrollTop >= header.offsetHeight * 3) {
+    if (scrollTop >= header.offsetHeight * 3) {
       header.classList.add("fixed")
-      body.style.marginTop =`${header.offsetHeight}px`
+      body.style.marginTop = `${header.offsetHeight}px`
     } else {
       header.classList.remove("fixed")
       body.style.marginTop = `0px`
@@ -58,9 +58,9 @@ window.addEventListener('DOMContentLoaded', () => {
   })
 
   // Menu
-  if(navPage) {
+  if (navPage) {
 
-    navToggle.addEventListener('click', function() {
+    navToggle.addEventListener('click', function () {
       if (navPage.classList.contains('active')) {
         navPage.classList.remove('active');
       } else {
@@ -68,7 +68,7 @@ window.addEventListener('DOMContentLoaded', () => {
       }
     });
 
-    navClosed.addEventListener('click', function() {
+    navClosed.addEventListener('click', function () {
       if (navPage.classList.contains('active')) {
         navPage.classList.remove('active');
       }
@@ -88,7 +88,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // current-item в footer
 
-  if(navFooter) {
+  if (navFooter) {
     const links = navFooter.getElementsByTagName('a');
     for (let i = 0; i < links.length; i++) {
 
@@ -100,7 +100,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   //Slider
-  if(document.querySelector(".courses-slider")) {
+  if (document.querySelector(".courses-slider")) {
 
     let swiper = new Swiper(".courses-slider", {
       slidesPerView: 3,
@@ -114,21 +114,21 @@ window.addEventListener('DOMContentLoaded', () => {
       breakpoints: {
         300: {
           slidesPerView: 1,
-       },
+        },
         480: {
-           slidesPerView: 1,
+          slidesPerView: 1,
         },
         768: {
-           slidesPerView: 2,
+          slidesPerView: 2,
         },
         1280: {
-           slidesPerView: 3,
+          slidesPerView: 3,
         }
-     }
+      }
     });
   }
 
-  if(document.querySelector(".professors-slider")) {
+  if (document.querySelector(".professors-slider")) {
 
     let swiper2 = new Swiper(".professors-slider", {
       slidesPerView: 3,
@@ -142,24 +142,24 @@ window.addEventListener('DOMContentLoaded', () => {
       breakpoints: {
         320: {
           slidesPerView: 1,
-       },
+        },
         480: {
-           slidesPerView: 1,
+          slidesPerView: 1,
         },
         768: {
-           slidesPerView: 1.5,
+          slidesPerView: 1.5,
         },
         900: {
           slidesPerView: 1.8,
-       },
+        },
         1280: {
-           slidesPerView: 2.5,
+          slidesPerView: 2.5,
         }
-     }
+      }
     });
   }
 
-  if(document.querySelector(".swiper-reviews")) {
+  if (document.querySelector(".swiper-reviews")) {
     let swiper3 = new Swiper(".swiper-reviews", {
       slidesPerView: 2.8,
       spaceBetween: 25,
@@ -167,68 +167,68 @@ window.addEventListener('DOMContentLoaded', () => {
       breakpoints: {
         320: {
           slidesPerView: 1,
-       },
+        },
         480: {
-           slidesPerView: 1,
+          slidesPerView: 1,
         },
         768: {
-           slidesPerView: 1.5,
+          slidesPerView: 1.5,
         },
         900: {
           slidesPerView: 2,
-       },
+        },
         1200: {
-           slidesPerView: 2.5,
+          slidesPerView: 2.5,
         }
-     }
+      }
     });
   }
 
-  if(document.querySelector(".course-teacher__slider")) {
+  if (document.querySelector(".course-teacher__slider")) {
     let swiper4 = new Swiper(".course-teacher__slider", {
       effect: "coverflow",
-        grabCursor: true,
-        centeredSlides: false,
-        initialSlide: 0,
-        slidesPerView: "auto",
-        coverflowEffect: {
-          rotate: 10,
-          stretch: 500,
-          depth: 200,
-          modifier: 1,
-          slideShadows: true,
+      grabCursor: true,
+      centeredSlides: false,
+      initialSlide: 0,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 10,
+        stretch: 500,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+      },
+      loop: true,
+      breakpoints: {
+        400: {
+          coverflowEffect: {
+            stretch: 100,
+            rotate: 10,
+            depth: 200,
+            modifier: 1,
+          },
         },
-        loop: true,
-        breakpoints: {
-          400: {
-            coverflowEffect: {
-              stretch: 100,
-              rotate: 10,
-              depth: 200,
-              modifier: 1,
-            },
+        800: {
+          coverflowEffect: {
+            stretch: 300,
+            rotate: 10,
+            depth: 200,
+            modifier: 1,
           },
-          800: {
-            coverflowEffect: {
-              stretch: 300,
-              rotate: 10,
-              depth: 200,
-              modifier: 1,
-            },
+        },
+        1300: {
+          coverflowEffect: {
+            rotate: 10,
+            stretch: 600,
+            depth: 200,
+            modifier: 1,
           },
-          1300: {
-            coverflowEffect: {
-              rotate: 10,
-              stretch: 600,
-              depth: 200,
-              modifier: 1,
-            },
-          },
-        }
+        },
+      }
     });
   }
 
-  if(document.querySelector(".page-courses-slider")) {
+  if (document.querySelector(".page-courses-slider")) {
     let swiper5 = new Swiper(".page-courses-slider", {
       slidesPerView: "auto",
       spaceBetween: 20,
@@ -246,15 +246,15 @@ window.addEventListener('DOMContentLoaded', () => {
       breakpoints: {
         320: {
           slidesPerView: 1,
-       },
+        },
         768: {
-           slidesPerView: "auto",
+          slidesPerView: "auto",
         },
       }
     });
   }
 
-  if(document.querySelector(".teacher-page-sliders")) {
+  if (document.querySelector(".teacher-page-sliders")) {
     let swiper6 = new Swiper(".sliders-faculty-one", {
       slidesPerView: 3,
       spaceBetween: 10,
@@ -267,20 +267,20 @@ window.addEventListener('DOMContentLoaded', () => {
       breakpoints: {
         320: {
           slidesPerView: 1,
-       },
+        },
         480: {
-           slidesPerView: 1,
+          slidesPerView: 1,
         },
         768: {
-           slidesPerView: 1,
+          slidesPerView: 1,
         },
         900: {
           slidesPerView: 2,
-       },
+        },
         1280: {
-           slidesPerView: 3,
+          slidesPerView: 3,
         }
-     }
+      }
     });
 
     let swiper7 = new Swiper(".sliders-faculty-second", {
@@ -295,24 +295,24 @@ window.addEventListener('DOMContentLoaded', () => {
       breakpoints: {
         320: {
           slidesPerView: 1,
-       },
+        },
         480: {
-           slidesPerView: 1,
+          slidesPerView: 1,
         },
         768: {
-           slidesPerView: 1,
+          slidesPerView: 1,
         },
         900: {
           slidesPerView: 2,
-       },
+        },
         1300: {
-           slidesPerView: 3,
+          slidesPerView: 3,
         }
-     }
+      }
     });
   }
 
-  if(document.querySelector(".about-gallery-wrapper")) {
+  if (document.querySelector(".about-gallery-wrapper")) {
     let swiper8 = new Swiper(".about-gallery-slider", {
       slidesPerView: 3,
       spaceBetween: 30,
@@ -326,32 +326,32 @@ window.addEventListener('DOMContentLoaded', () => {
       breakpoints: {
         300: {
           slidesPerView: 1,
-       },
+        },
         480: {
-           slidesPerView: 1,
+          slidesPerView: 1,
         },
         768: {
-           slidesPerView: 1,
+          slidesPerView: 1,
         },
         900: {
           slidesPerView: 2,
-       },
+        },
         1280: {
-           slidesPerView: 3,
+          slidesPerView: 3,
         }
-     }
+      }
     });
 
   }
 
   //Accordeon
 
-  if(document.querySelector(".accordeon-list")) {
+  if (document.querySelector(".accordeon-list")) {
 
     const accordeonItems = document.querySelectorAll(".accordion-list__item");
 
     for (let i = 0; i < accordeonItems.length; i++) {
-      accordeonItems[i].addEventListener('click', function(el) {
+      accordeonItems[i].addEventListener('click', function (el) {
 
         el.currentTarget.classList.toggle('active');
 
@@ -366,13 +366,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
   //Tabs
 
-  if(document.querySelector(".section-tabs")) {
+  if (document.querySelector(".section-tabs")) {
 
     const tabs = document.querySelectorAll(".tabs-button ");
     const contents = document.querySelectorAll(".tabs-panel");
 
     for (let i = 0; i < tabs.length; i++) {
-      tabs[i].addEventListener("click", ( event ) => {
+      tabs[i].addEventListener("click", (event) => {
 
         let tabsChildren = event.target.parentElement.children;
         console.log(tabsChildren)
@@ -392,13 +392,13 @@ window.addEventListener('DOMContentLoaded', () => {
     }
   }
 
-  if(document.querySelector(".section-courses-slider")) {
+  if (document.querySelector(".section-courses-slider")) {
 
     const tabs = document.querySelectorAll(".section-directions__item");
     const contents = document.querySelectorAll(".section-directions-panel");
 
     for (let i = 0; i < tabs.length; i++) {
-      tabs[i].addEventListener("click", ( event ) => {
+      tabs[i].addEventListener("click", (event) => {
 
         let tabsChildren = event.target.parentElement.children;
         console.log(tabsChildren)
@@ -421,15 +421,15 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   //Modal
-  if(document.querySelector(".thanks-content")) {
-    document.addEventListener("DOMContentLoaded", function(){
+  if (document.querySelector(".thanks-content")) {
+    document.addEventListener("DOMContentLoaded", function () {
       let scrollbar = document.body.clientWidth - window.innerWidth + 'px';
       console.log(scrollbar);
-      document.querySelector('[href="#openModal"]').addEventListener('click',function(){
+      document.querySelector('[href="#openModal"]').addEventListener('click', function () {
         document.body.style.overflow = 'hidden';
         document.querySelector('#openModal').style.marginLeft = scrollbar;
       });
-      document.querySelector('[href="#close"]').addEventListener('click',function(){
+      document.querySelector('[href="#close"]').addEventListener('click', function () {
         document.body.style.overflow = 'visible';
         document.querySelector('#openModal').style.marginLeft = '0px';
       });
@@ -437,7 +437,7 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   // Progress-bar
-  if(document.querySelector(".progress-bar")) {
+  if (document.querySelector(".progress-bar")) {
 
     function progressBar() {
       let historyContainer = document.querySelector(".about-history")
@@ -447,7 +447,7 @@ window.addEventListener('DOMContentLoaded', () => {
       let progressNow = document.querySelector(".progress-bar-now")
       progressNow.style.height = scrolled + '%';
 
-      if(scrolled >= 50) {
+      if (scrolled >= 50) {
         document.querySelector(".progress-dots__second").classList.add("dots-active")
       } else {
         document.querySelector(".progress-dots__second").classList.remove("dots-active")
@@ -459,7 +459,7 @@ window.addEventListener('DOMContentLoaded', () => {
         document.querySelector(".progress-dots__third").classList.remove("dots-active")
         progressNow.style.height = scrolled + '%';
       }
-  }
+    }
 
     window.addEventListener('scroll', progressBar);
   }
